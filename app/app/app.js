95228a8ef7dd00a10516013f55e961fe07d0d653
Matuzalém Teles <matuzalemteles@gmail.com>
2015-08-26 13:29:06 -0300

    First comment

---
+++ app/app/app.js
@@ -0,0 +1,65 @@
+var app = angular.module('app',['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){   
+   // remove o # da url
+   //$locationProvider.html5Mode({
+   //    enabled: true,
+   //    requireBase: false
+   //});
+
+   $routeProvider
+ 
+   // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
+   .when('/', {
+      title       : 'Kep Framework for PHP',
+      templateUrl : 'app/views/home.html',
+      controller  : 'homeController',
+   })
+   .when('/docs', {
+      title       : 'Documentation - Kep Framework for PHP',
+      templateUrl : 'app/views/docs.html',
+      controller  : 'docsController',
+   })
+   .when('/docs/routing', {
+      title       : 'Routing - Kep Framework for PHP',
+      templateUrl : 'app/views/routing.html',
+      controller  : 'routingController',
+   })
+   .when('/docs/controller', {
+      title       : 'Controller - Kep Framework for PHP',
+      templateUrl : 'app/views/controller.html',
+      controller  : 'controllerController',
+   })
+   .when('/docs/model', {
+      title       : 'Model - Kep Framework for PHP',
+      templateUrl : 'app/views/model.html',
+      controller  : 'modelController',
+   })
+   .when('/docs/query-builder', {
+      title       : 'Query Builder - Kep Framework for PHP',
+      templateUrl : 'app/views/query-builder.html',
+      controller  : 'querybuilderController',
+   })
+   .when('/docs/seeds', {
+      title       : 'Seeds - Kep Framework for PHP',
+      templateUrl : 'app/views/seeds.html',
+      controller  : 'seedsController',
+   })
+   .when('/docs/angularjs', {
+      title       : 'AngularJS - Kep Framework for PHP',
+      templateUrl : 'app/views/angularjs.html',
+      controller  : 'angularjsController',
+   })
+ 
+   // caso não seja nenhum desses, redirecione para a rota '/'
+   .otherwise ({ redirectTo: '/' });
+});
+
+app.run(function($rootScope, $templateCache) {
+   $rootScope.$on('$viewContentLoaded', function() {
+      $templateCache.removeAll();
+   });
+   $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+   });
+});
