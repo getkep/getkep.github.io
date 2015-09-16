f3c3a5f4aed8554ca22e8031acfa6099abef7169
Matuzalém Teles <matuzalemteles@gmail.com>
2015-09-16 18:03:10 -0300

    Iniciando funções de compra

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -39,5 +39,37 @@ app.controller('authenticationController', function($scope, $http, $location){
 });
 
 app.controller('storeController', function($scope, $http, $location){
-
+	$scope.jsonCheckout = [
+		{
+			value: 0,
+			products: {
+				stk001: 0,
+				stk002: 0,
+				stk003: 0,
+			}
+		}
+	];
+	
+	$scope.value = 0.00;
+	
+	$scope.isMsg = false;
+	$scope.Msg = null;
+	
+	$scope.buy = function (params) {
+		$scope.value = $scope.value + 5.99;
+		
+		$scope.jsonCheckout.value = $scope.value;
+		$scope.jsonCheckout[0].products[params] = $scope.jsonCheckout[0].products[params] + 1;
+		console.log($scope.jsonCheckout);
+	}
+	
+	$scope.checkout = function(){
+		if($scope.value == 0.00){
+			$scope.isMsg = true;
+			$scope.Msg = "O carrinho está vazio :/";
+		}else{
+			$scope.isMsg = true;
+			$scope.Msg = "Ficamos felizes pelo o seu interesse, mais a loja não está pronta ainda :/"
+		}
+	}
 });
